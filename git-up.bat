dbc8ab627acd264d9e998789322d02c455838c10
Mohammad Moinul islam <mohammadmoinul.islam@outlook.com>
2021-11-24 11:54:18 +1100

    Wed 24/11/2021-11:54:18.81

--- git-up.bat
+++ git-up.bat
@@ -1,5 +1,6 @@
 git add .
 git commit -am " %date%-%time% "
 rem git remote add origin https://mohammadmoinulislam0115@dev.azure.com/mohammadmoinulislam0115/C-sharp/_git/C-sharp
+git remote set-url origin https://github.com/momizgit/my-c-sh.git
 git remote add origin https://github.com/momizgit/my-c-sh.git
 git push -u origin --all
